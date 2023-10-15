8a153e3fe66aa1b1faf5797f2536a7c1f0146bdd
Masterain <i@irain.in>
2023-10-14 19:23:48 -0700

    Create tengine.py

---
+++ utils/tengine.py
@@ -0,0 +1,17 @@
+import httpx
+
+
+def make_cache() -> list:
+    resource_list = []
+
+    url = "https://api.github.com/repos/alibaba/tengine/releases"
+    full_release_list = httpx.get(url).json()
+    non_pre_release_list = [release for release in full_release_list if not release["prerelease"]]
+    tag_list = [release["tag_name"] for release in non_pre_release_list]
+    for tag in tag_list:
+        tag_tar_gz = f"https://github.com/alibaba/tengine/archive/refs/tags/{tag}.tar.gz"
+        resource_list.append({
+            "url": tag_tar_gz,
+            "file_name": tag_tar_gz
+        })
+    return resource_list
